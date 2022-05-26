41dff52eff649229f23f860e1bcd0627e53f9f13
Susan Boyd <sueboyd922@gmail.com>
2022-05-25 21:50:06 -0400

    Tests for items index page

---
+++ spec/features/items/index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'items index page' do
+  it 'has a list of all the items' do
+    visit "/items"
+
+    expect(page).to have_link("Item Nemo Facere")
+    expect(page).to have_link("Item Expedita Aliquam")
+    expect(page).to have_link("Item Provident At")
+  end
+
+  it 'links to the items show page' do
+    visit "/items"
+
+    click_on("Item Nemo Facere")
+    expect(current_path).to eq("/items/4")
+  end
+end
